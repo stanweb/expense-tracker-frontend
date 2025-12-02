e29f3c9829d601f3b9b22e1b45b74ad49124e683
Stanley Kamau <skamau@dtbafrica.com>
2025-12-02 20:23:29 +0300

    Modify the register page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -45,14 +45,14 @@ const LoginPage = () => {
 
     return (
         <div className="flex min-h-screen min-w-screen items-center flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-card">
-            <div className={"rounded-3xl shadow-2xl px-4 py-2 sm:w-full md:w-[40vw]"} >
+            <div className={"rounded-3xl shadow-2xl px-4 py-2 sm:w-full md:w-[25vw]"} >
                 <div className="">
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
                 </div>
 
-                <div className="mt-10 sm:mx-auto">
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" onSubmit={handleSubmit}>
                         <div>
                             <Label
@@ -103,7 +103,7 @@ const LoginPage = () => {
                         <div>
                             <Button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Sign in
                             </Button>
